f007a3d088d3f9a69eccde7cbae2fb24a13a5325
HIDEKAZU AOSHIMA <h.aoshima@livlog.jp>
2023-01-30 14:52:22 +0900

    no message

--- src/jp/livlog/gpt3sample/Sample1.java
+++ src/jp/livlog/gpt3sample/Sample1.java
@@ -1,23 +1,38 @@
 package jp.livlog.gpt3sample;
 
+import java.util.ResourceBundle;
+
 import com.theokanning.openai.OpenAiService;
+import com.theokanning.openai.completion.CompletionChoice;
 import com.theokanning.openai.completion.CompletionRequest;
 
 public class Sample1 {
 
     public static void main(final String[] args) {
 
-        final var token = System.getenv("OPENAI_TOKEN");
-        final OpenAiService service = new OpenAiService(token);
+        final var config = ResourceBundle.getBundle("config");
+
+        final var token = config.getString("openai.token");
+        final var service = new OpenAiService(token);
 
         System.out.println("\nCreating completion...");
+
+        final var message = "ラーメンは健康にいい食べ物ですよね。";
+        final var prompt = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever.\nHuman: " + message
+                + "\nAI: ";
+
         final var completionRequest = CompletionRequest.builder()
-                .model("ada")
-                .prompt("Somebody once told me the world is gonna roll me")
-                .echo(true)
-                .user("testing")
+                .model("text-davinci-003")
+                .prompt(prompt)
+                .maxTokens(256)
                 .build();
-        service.createCompletion(completionRequest).getChoices().forEach(System.out::println);
+        final var completionResult = service.createCompletion(completionRequest);
+        final var choiceList = completionResult.getChoices();
+
+        for (final CompletionChoice choice : choiceList) {
+            System.out.println(choice);
+        }
+
     }
 
 }
